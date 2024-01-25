629f8bd6410ed43aafd654316bfb56ea7b5ab581
Wiktor <krolwofficial@gmail.com>
2024-01-25 21:49:19 +0100

    test commit

--- 014-objects/exercises/exercise-3.js
+++ 014-objects/exercises/exercise-3.js
@@ -21,3 +21,12 @@
 
     *This is a challenging exercise - take it slow and step by step
 */
+
+const houseForSale = {
+  area: 940,
+  value: 320000,
+  streetName: "Fifth Street",
+  built: "2012",
+  owner: { name: "Blake", age: 29 },
+  offers: [290000, 295000, 315000, 312000],
+};
